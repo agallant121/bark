831b7d568d4a29e19c373af0291e5587d677de78
Alex Gallant <agallant1201@gmail.com>
2020-08-07 13:42:41 -0600

    Add restaurant model with validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,19 @@
 class Restaurant < ApplicationRecord
+  validates_presence_of :name
+
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+
+  def avg_rating
+    reviews.average(:rating).round(1)
+  end
+
+  def order_reviews
+    reviews.order(created_at: :desc)
+    # reviews.order("created_at DESC")
+  end
+
+  def self.order_places
+    order(avg_rating: :desc)
+  end
 end

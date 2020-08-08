68a35eacab26c24fccc9cecc737ee162ebd16313
Alex Gallant <agallant1201@gmail.com>
2020-08-08 15:35:02 -0600

    Add route to delete comments from reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   resources :restaurants do
-    resources :reviews
-      resources :comments, only: [:destroy]
+    resources :reviews do
+      # delete '/:id/comment', to: 'reviews#destroy_comment'
+    end
   end
+  patch '/restaurants/:restaurant_id/reviews/:review_id/comment', to: 'reviews#update'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

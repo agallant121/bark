04f7ebc4ea3e27abf870f14715b0321b4ec520f8
Alex Gallant <agallant1201@gmail.com>
2020-08-08 11:56:54 -0600

    Add pagination to the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.all.paginate(page: params[:page], per_page: 5)
   end
 end
